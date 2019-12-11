d53874a3f346a2bea816a8a24953ffefbe31818b
HelenMatveevaN <30690551+HelenMatveevaN@users.noreply.github.com>
2019-12-11 17:48:14 +0300

    Update for.py

--- for.py
+++ for.py
@@ -1,13 +1,10 @@
 '''
-
 Оценки
-
 Создать список из словарей с оценками учеников разных классов школы вида [{'school_class': '4a', 'scores': [3,4,4,5,2]}, ...]
 Посчитать и вывести средний балл по всей школе.
 Посчитать и вывести средний балл по каждому классу.
-
 '''
-def main:
+def main():
     listp = [
         {'school_class': '4a', 'scores': [3,4,4,5,2]},
         {'school_class': '5b', 'scores': [3,3,5,4,5]},
@@ -16,11 +13,14 @@ def main:
     score_cnt = 0
     score_sum = 0
     for score in listp:
-        score_cnt = score_cnt + score['scores'].len
-        score_sum = score_sum + score['scores'].sum
-        print("средний балл по классу {score['school_class']}: {score['scores'].sum/score['scores'].len}")
-    
-    print("средний балл по всей школе: { score_sum/score_cnt }")
+        class_ = score['school_class']
+        score1_cnt = len(score['scores'])#длина списка
+        score_cnt += score1_cnt
+        score1_sum = sum(score['scores'])#сумма элементов списка
+        score_sum += score1_sum
+        print('средний балл по классу {}: {}'.format(class_, score1_sum/score1_cnt))
+
+    print("средний балл по всей школе: {}".format(score_sum/score_cnt))
 
 if __name__ == "__main__":
     main()
